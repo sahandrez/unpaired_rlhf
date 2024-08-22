d8777c11e795bcd87bec608c7c89504177504d03
Sahand Rezaei-Shoshtari <sahand.rezaei.sh@gmail.com>
2024-08-21 20:05:42 -0400

    Small change to the bash script

--- jobs_local/train_pairwise_reward.sh
+++ jobs_local/train_pairwise_reward.sh
@@ -3,12 +3,12 @@
 python pairwise_reward_model.py \
     --model_name_or_path=alignment-handbook/zephyr-7b-sft-qlora \
     --dataset_name=Anthropic/hh-rlhf \
-    --output_dir=logs/reward-modeling-zephyr-7b-sft-qlora \
+    --output_dir=logs/pairwise-reward-zephyr-7b-sft-qlora \
     --torch_dtype=bfloat16 \
     --attn_implementation=flash_attention_2 \
     --per_device_train_batch_size=32 \
     --per_device_eval_batch_size=32 \
-    --gradient_accumulation_steps=2 \
+    --gradient_accumulation_steps=1 \
     --num_train_epochs=1 \
     --learning_rate=1.5e-5 \
     --max_length=512 \
