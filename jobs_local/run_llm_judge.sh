2dd1a2da6748b6ae4b89cdec636807f1fa087cbc
saminyeasar <samin.arnob@mail.mcgill.ca>
2024-08-23 03:04:06 +0000

    added scripts to judge llms

---
+++ jobs_local/run_llm_judge.sh
@@ -0,0 +1,7 @@
+
+
+# Llama3 70B model
+#python llm_judge.py --model_name='meta-llama/Meta-Llama-3.1-70B-Instruct' --model_dtype = "bfloat16"
+
+# Llama3 8B model
+python llm_judge.py --model_name='meta-llama/Meta-Llama-3.1-8b-Instruct'
