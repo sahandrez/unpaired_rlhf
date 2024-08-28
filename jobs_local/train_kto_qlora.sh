e7ab9b7edf15fd821b67a6f65b02b1405281163d
Sahand Rezaei-Shoshtari <sahand.rezaei.sh@gmail.com>
2024-08-27 20:19:42 -0400

    Format the job scripts

--- jobs_local/train_kto_qlora.sh
+++ jobs_local/train_kto_qlora.sh
@@ -1,32 +1,32 @@
 # Trains Zephyr 7B with KTO using QLoRA
 
 python kto.py \
-    --model_name_or_path=alignment-handbook/zephyr-7b-sft-qlora \
-    --dataset_name=sahandrez/ultrafeedback_binarized_unpaired \
-    --output_dir=logs/kto-zephyr-7b-sft-qlora \
-    --torch_dtype=bfloat16 \
-    --attn_implementation=flash_attention_2 \
-    --per_device_train_batch_size=4 \
-    --per_device_eval_batch_size=8 \
+    --model_name_or_path alignment-handbook/zephyr-7b-sft-qlora \
+    --dataset_name sahandrez/ultrafeedback_binarized_unpaired \
+    --output_dir logs/kto-zephyr-7b-sft-qlora \
+    --torch_dtype bfloat16 \
+    --attn_implementation flash_attention_2 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 8 \
     --gradient_accumulation_steps 4 \
-    --num_train_epochs=1 \
-    --learning_rate=5.0e-6 \
-    --lr_scheduler_type=cosine \
-    --max_length=1024 \
-    --max_prompt_length=512 \
-    --optim=paged_adamw_32bit \
+    --num_train_epochs 1 \
+    --learning_rate 5.0e-6 \
+    --lr_scheduler_type cosine \
+    --max_length 1024 \
+    --max_prompt_length 512 \
+    --optim paged_adamw_32bit \
     --gradient_checkpointing \
-    --logging_steps=10 \
-    --evaluation_strategy=steps \
-    --eval_steps=500 \
-    --warmup_ratio=0.1 \
-    --report_to=wandb \
+    --logging_steps 10 \
+    --evaluation_strategy steps \
+    --eval_steps 500 \
+    --warmup_ratio 0.1 \
+    --report_to wandb \
     --push_to_hub \
     --bf16 \
     --logging_first_step \
     --use_peft \
     --load_in_4bit \
-    --lora_r=128 \
-    --lora_alpha=128 \
-    --lora_dropout=0.05 \
+    --lora_r 128 \
+    --lora_alpha 128 \
+    --lora_dropout 0.05 \
     --lora_target_modules q_proj k_proj v_proj o_proj gate_proj up_proj down_proj
--- jobs_local/train_pairwise_reward.sh
+++ jobs_local/train_pairwise_reward.sh
@@ -1,34 +1,34 @@
 # Trains a pairwise reward model
 
 python pairwise_reward_model.py \
-    --model_name_or_path=alignment-handbook/zephyr-7b-sft-qlora \
-    --dataset_name=HuggingFaceH4/ultrafeedback_binarized \
-    --train_split="train_prefs" \
-    --test_split="test_prefs" \
-    --output_dir=logs/pairwise-reward-zephyr-7b-sft-qlora \
-    --torch_dtype=bfloat16 \
-    --attn_implementation=flash_attention_2 \
-    --per_device_train_batch_size=32 \
-    --per_device_eval_batch_size=32 \
-    --gradient_accumulation_steps=1 \
-    --num_train_epochs=1 \
-    --learning_rate=1.5e-5 \
-    --max_length=512 \
-    --remove_unused_columns=False \
-    --optim=adamw_torch \
+    --model_name_or_path alignment-handbook/zephyr-7b-sft-qlora \
+    --dataset_name HuggingFaceH4/ultrafeedback_binarized \
+    --train_split "train_prefs" \
+    --test_split "test_prefs" \
+    --output_dir logs/pairwise-reward-zephyr-7b-sft-qlora \
+    --torch_dtype bfloat16 \
+    --attn_implementation flash_attention_2 \
+    --per_device_train_batch_size 32 \
+    --per_device_eval_batch_size 32 \
+    --gradient_accumulation_steps 1 \
+    --num_train_epochs 1 \
+    --learning_rate 1.5e-5 \
+    --max_length 512 \
+    --remove_unused_columns False \
+    --optim adamw_torch \
     --gradient_checkpointing \
-    --logging_steps=10 \
-    --eval_strategy=steps \
-    --eval_steps=100 \
+    --logging_steps 10 \
+    --eval_strategy steps \
+    --eval_steps 100 \
     --load_best_model_at_end \
-    --report_to=wandb \
+    --report_to wandb \
     --push_to_hub \
     --bf16 \
     --logging_first_step \
     --use_peft \
     --load_in_4bit \
     --lora_task_type SEQ_CLS \
-    --lora_r=128 \
-    --lora_alpha=128 \
-    --lora_dropout=0.05 \
+    --lora_r 128 \
+    --lora_alpha 128 \
+    --lora_dropout 0.05 \
     --lora_target_modules q_proj k_proj v_proj o_proj gate_proj up_proj down_proj
