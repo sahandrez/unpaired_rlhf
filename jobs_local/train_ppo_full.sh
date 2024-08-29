9c58c766ba650444ee0bca1bc70bc25a05b7f6f7
Sahand Rezaei-Shoshtari <sahand.rezaei.sh@gmail.com>
2024-08-29 17:00:22 -0400

    Add a full script for pythia

---
+++ jobs_local/train_ppo_full.sh
@@ -0,0 +1,23 @@
+python ppo.py \
+    --model_name_or_path EleutherAI/pythia-1b-deduped \
+    --sft_model_path EleutherAI/pythia-1b-deduped \
+    --reward_model_path EleutherAI/pythia-1b-deduped \
+    --dataset_name HuggingFaceH4/ultrafeedback_binarized \
+    --train_split "train_prefs" \
+    --test_split "test_prefs" \
+    --output_dir logs/ppo-pythia-1b-deduped \
+    --num_ppo_epochs 1 \
+    --num_mini_batches 1 \
+    --learning_rate 3e-6 \
+    --attn_implementation flash_attention_2 \
+    --torch_dtype bfloat16 \
+    --per_device_train_batch_size 1 \
+    --gradient_accumulation_steps 16 \
+    --gradient_checkpointing \
+    --total_episodes 10000 \
+    --local_rollout_forward_batch_size 1 \
+    --non_eos_penalty \
+    --report_to wandb \
+    --push_to_hub \
+    --bf16 \
+    --logging_first_step \
